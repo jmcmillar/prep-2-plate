37adeb20a3fc23e2ae3ea319264eb78788be9252
Joyce Clay <jmmcmillar@gmail.com>
2025-11-22 23:25:42 -0600

    adds update and destroy for lists on api

--- app/controllers/api/shopping_lists_controller.rb
+++ app/controllers/api/shopping_lists_controller.rb
@@ -13,6 +13,16 @@ class Api::ShoppingListsController < Api::BaseController
     end
   end
 
+  def update
+    @shopping_list = Current.user.shopping_lists.find(params[:id])
+
+    if @shopping_list.update(shopping_list_params)
+      render json: @shopping_list, status: :ok
+    else
+      render json: { errors: @shopping_list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @shopping_list = Current.user.shopping_lists.find(params[:id])
 
@@ -21,5 +31,11 @@ class Api::ShoppingListsController < Api::BaseController
     else
       render json: @shopping_list.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:name, :current)
   end
 end
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,22 +2,22 @@ namespace :api do
   post "auth/sign_in", to: "auth#create"
   post "auth/sign_up", to: "auth_registration#create"
   delete "auth/sign_out", to: "auth#destroy"
-  resource :home, only: [:show]
+  resource :home, only: [ :show ]
   resources :categories, only: %i[index], controller: "filters/categories"
   resources :meal_types, only: %i[index], controller: "filters/meal_types"
-  resources :measurement_units, only: [:index]
-  resources :recipes, only: [:index, :show, :create]
-  resources :recipe_ingredients, only: [:index]
-  resource :recipe_imports, only: [:show, :create]
-  resources :current_shopping_lists, only: [:create]
-  resource :user_notifications, only: [:update, :show]
-  resources :recipe_favorites, only: [:index, :create]
-  resources :shopping_lists, only: [:index, :create, :destroy] do
-    resources :shopping_list_items, only: [:index, :create, :destroy], shallow: true
+  resources :measurement_units, only: [ :index ]
+  resources :recipes, only: [ :index, :show, :create ]
+  resources :recipe_ingredients, only: [ :index ]
+  resource :recipe_imports, only: [ :show, :create ]
+  resources :current_shopping_lists, only: [ :create ]
+  resource :user_notifications, only: [ :update, :show ]
+  resources :recipe_favorites, only: [ :index, :create ]
+  resources :shopping_lists, only: [ :index, :create, :update, :destroy ] do
+    resources :shopping_list_items, only: [ :index, :create, :destroy ], shallow: true
   end
-  resource :export_meal_plans, only: [:create]
-  resources :recipe_categories, only: [:index, :show]
+  resource :export_meal_plans, only: [ :create ]
+  resources :recipe_categories, only: [ :index, :show ]
   resource :user_details, only: %i[show update]
   resource :user_password, only: %i[update]
-  resources :meal_plans, only: [:create, :update, :destroy]
+  resources :meal_plans, only: [ :create, :update, :destroy ]
 end
