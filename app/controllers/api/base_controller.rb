44bfba19e3cfdb2d6dcf28aeff3d789b074190dd
Joyce Clay <jmmcmillar@gmail.com>
2025-12-14 12:30:05 -0600

    quick patch for android devices, 406 being returned

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,6 @@
 class Api::BaseController < ActionController::API
   include Api::TokenAuthentication
+  before_action :set_default_format
   
   rescue_from ActionController::ParameterMissing, with: :parameter_missing
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
@@ -25,5 +26,10 @@ class Api::BaseController < ActionController::API
       status: 403,
       message: "You are not authorized to perform this action."
     }, status: :forbidden
+  end
+
+  # temp fix until all the requests accept json
+  def set_default_format
+    request.format = :json if request.format == '*/*' || request.format.html?
   end
 end
